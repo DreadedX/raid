d2591a6e48d5bf583fea8e3faf1eb8adcce8c7e7
Dreaded_X <tim.huizinga@gmail.com>
2016-10-10 02:51:15 +0200

    Restucture of flx2 reader/writer

--- src/flxr/write.cpp
+++ src/flxr/write.cpp
@@ -9,57 +9,69 @@
 
 using namespace flxr;
 //----------------------------------------------
+const int32 MAGIC = 844647494; /* FLX2 */
+//----------------------------------------------
 enum COMPRESSION : byte {
 	ZLIB,
 };
 //----------------------------------------------
-class File {
-	public:
-		File(std::string m_name) : name(m_name) {
-			offset = 30;
-		}
+struct File {
+	File(std::string m_name) : name(m_name) {}
 
-		auto get_name() { return name; }
-		auto get_offset() { return offset; }
-		auto get_size() { return reinterpret_cast<uint64>(data.size()); }
+	std::string name;
+	/// @todo Calculate this correctly
+	uint64 offset = 30;
+	std::vector<byte> data;
+	uint64 size;
 
-		auto get_data() {
-			data = {0x44,0x41,0x54,0x41};
+	auto get_size() {
+		size = reinterpret_cast<uint64>(data.size());
+		return size;
+	}
 
-			return data;
-		}
+	auto get_data() {
+		data = {0x44,0x41,0x54,0x41};
 
-	private:
-		std::string name;
-		uint64 offset;
-		std::vector<byte> data;
+		return data;
+	}
 };
 //----------------------------------------------
-class Container {
-	public:
-		Container(std::string m_name) : name(m_name) {}
+struct Container {
+	std::string name;
+	std::vector<File> files;
+	#pragma pack(1)
+	struct {
+		int32 magic;
+		COMPRESSION compression;
+		int64 file_count;
+	} header;
 
-		auto get_name() { return name; }
-		auto get_file_count() { return reinterpret_cast<uint64>(files.size()); }
-		auto get_files() { return files; }
+	void configure(COMPRESSION m_compression) {
+		header.magic = MAGIC;
+		header.compression = m_compression;
+		header.file_count = reinterpret_cast<uint64>(files.size());
+	}
 
-		void add_file(File file) { files.push_back(std::move(file)); }
-
-	private:
-		std::string name;
-		std::vector<File> files;
+	void add_file(File file) {
+		files.push_back(std::move(file));
+	}
 };
-//----------------------------------------------
-const int32 MAGIC = 844647494; /* FLX2 */
 //----------------------------------------------
 std::ostream& write_header(std::ostream& stream, Container& container) {
-	write(stream, MAGIC);
-	write(stream, COMPRESSION::ZLIB);
-	write(stream, container.get_file_count());
+	// write(stream, container.get_header().magic);
+	// write(stream, container.get_header().compression);
+	// write(stream, container.get_header().file_count);
+	write(stream, container.header);
 
-	for(auto file : container.get_files()) {
-		write(stream, file.get_name());
-		write(stream, file.get_offset());
+	return stream;
+}
+//----------------------------------------------
+std::ostream& write_index(std::ostream& stream, Container& container) {
+	for(auto file : container.files) {
+		write(stream, file.name);
+		write(stream, file.offset);
+		/// @todo This is just to correctly set the size in the first place
+		file.get_data();
 		write(stream, file.get_size());
 	}
 
@@ -67,7 +79,7 @@ std::ostream& write_header(std::ostream& stream, Container& container) {
 }
 //----------------------------------------------
 std::ostream& write_data(std::ostream& stream, Container& container) {
-	for(auto file : container.get_files()) {
+	for(auto file : container.files) {
 		write(stream, file.get_data());
 	}
 
@@ -142,34 +154,29 @@ std::fstream& check_crc(std::fstream& stream) {
 }
 //----------------------------------------------
 /// @todo This is just for testing
-std::istream& read_header(std::istream& stream) {
-	int32 magic;
-	read(stream, magic);
-	std::cout << magic << '\n';
-	/// @todo Add error handling
-	if (magic != MAGIC) {
+std::istream& read_header(std::istream& stream, Container& container) {
+	read(stream, container.header);
+
+	if (container.header.magic != MAGIC) {
 		std::cout << "Invalid magic number\n";
 		exit(-1);
 	}
 
-	COMPRESSION compression;
-	read(stream, compression);
-	std::cout << compression << '\n';
-
-	uint64 file_count;
-	read(stream, file_count);
-	std::cout << file_count << '\n';
-
-	for (uint64 i = 0; i < file_count; i++) {
+	return stream;
+}
+//----------------------------------------------
+std::istream& read_index(std::istream& stream, Container& container) {
+	for (uint64 i = 0; i < container.header.file_count; i++) {
 
 		std::string file_name;
 		read(stream, file_name);
-		uint64 offset;
-		read(stream, offset);
-		uint64 size;
-		read(stream, size);
+		File file(file_name);
+		read(stream, file.offset);
+		read(stream, file.size);
 
-		std::cout << file_name << "(offset: " << offset << ", size: " << size << ")\n";
+		container.add_file(file);
+
+		std::cout << file.name << "(offset: " << file.offset << ", size: " << file.size << ")\n";
 	}
 
 	return stream;
@@ -178,20 +185,24 @@ std::istream& read_header(std::istream& stream) {
 int main() {
 	//-Writing--------------------------------------
 	std::cout << "WRITING\n";
-	Container container("test.flx");
+	Container container;
+	container.name = "test.flx";
 
 	container.add_file(File("test1"));
 	container.add_file(File("test2"));
 
+	container.configure(COMPRESSION::ZLIB);
+
 	std::fstream stream;
-	stream.open(container.get_name(), std::ios::out | std::ios::in | std::ios::trunc | std::ios::binary);
+	stream.open(container.name, std::ios::out | std::ios::in | std::ios::trunc | std::ios::binary);
 
 	if(!stream.is_open()) {
-		std::cout << "Failed to open file: " << container.get_name() << '\n';
+		std::cout << "Failed to open file: " << container.name << '\n';
 		exit(-1);
 	}
 
 	write_header(stream, container);
+	write_index(stream, container);
 	write_data(stream, container);
 	write_crc(stream);
 
@@ -199,12 +210,13 @@ int main() {
 
 	//----------------------------------------------
 	//-Reading--------------------------------------
+	Container read_container;
 	std::cout << "READING\n";
-	stream.open(container.get_name(), std::ios::in | std::ios::binary);
+	stream.open(container.name, std::ios::in | std::ios::binary);
 
 	check_crc(stream);
-
-	read_header(stream);
+	read_header(stream, read_container);
+	read_index(stream, read_container);
 
 	stream.close();
 	//----------------------------------------------
